9729c6d1906d7f8ea812fe7046a46a11a097eec2
xreider <49386751+xreider@users.noreply.github.com>
2021-01-23 00:11:59 +1000

    Update socket.client.js

--- plugins/socket.client.js
+++ plugins/socket.client.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+const store = require('../store')
 import VueSocketIO from 'vue-socket.io'
 
 export default function () {
  Vue.use(new VueSocketIO({
    debug: true,
    connection: '/',
+   vuex: {
+       store,
+       actionPrefix: 'SOCKET_',
+       mutationPrefix: 'SOCKET_'
+   },
  }))
 }
